206848e8ac09f7d0d15d394986004a59d89acced
enrico <saggiorato.enrico@gmail.com>
2023-04-19 01:48:38 +0200

    other files

--- src/config/parse/document.rs
+++
@@ -1,3 +0,0 @@
-use super::{super::Config, error::ParseError, line::ConfigLine};
-
-pub fn parse(document: Vec<ConfigLine>) -> Result<Config, Vec<ParseError>> {}
--- src/config/parse/error.rs
+++
@@ -1,7 +0,0 @@
-use super::line::LineErrorKind;
-
-pub struct ParseError {
-    line_number: u32,
-    line: String,
-    kind: LineErrorKind,
-}
